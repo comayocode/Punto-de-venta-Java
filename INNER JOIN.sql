fffaa6760988c6b07120b17344c5a09c956ace33
roko <ronalc726@gmail.com>
2022-09-03 22:57:00 -0500

    Eliminados por problemas de compatibilidad

--- INNER JOIN.sql
+++
@@ -1,5 +0,0 @@
-SELECT c.codigo, c.fecha, p.codigo, p.nombre, c.entrada_kg, p.precio_compra, p.precio_venta,
-c.total_compra, c.observaciones
-FROM add_products p
-INNER JOIN compras c
-ON p.codigo = c.codigo;
