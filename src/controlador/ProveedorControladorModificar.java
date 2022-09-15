687d498fcfe27a63f236c6b48fc9427f5fa0daed
roko <ronalc726@gmail.com>
2022-09-15 17:46:07 -0500

    style: Añadir comentarios a bloques de codigo
    Se agrego comentarios explicando que hace X parte de codigo

--- src/controlador/ProveedorControladorModificar.java
+++ src/controlador/ProveedorControladorModificar.java
@@ -9,61 +9,71 @@ import modelo.tablas.ProveedorTablaModelo;
 import modelo.vo.ProveedorVO;
 import vista.frmProveedoresModificar;
 
-public class ProveedorControladorModificar implements ActionListener{
-    
+public class ProveedorControladorModificar implements ActionListener {
+
     private ProveedorVO vo;
     private ProveedorDAO dao;
     private frmProveedoresModificar vista;
-    
-    public ProveedorControladorModificar (ProveedorVO vo, ProveedorDAO dao, frmProveedoresModificar vista){
-        
+
+    //Constructor de la clase
+    public ProveedorControladorModificar(ProveedorVO vo, ProveedorDAO dao, frmProveedoresModificar vista) {
+
         this.vo = vo;
         this.dao = dao;
         this.vista = vista;
-        
+
         this.vista.btnModificar.addActionListener(this);
-        
     }
-    
+
+    //Posición y titulo de ventana de windows
     public void iniciar() {
         vista.setLocationRelativeTo(null);
-        vista.setTitle("Añadir Productos");
+        vista.setTitle("Modificar Productos");
     }
-    
+
     public void actionPerformed(ActionEvent evn) {
-    
+
+        //Al dar clic en btnModificar, pasa los datos de los campos al vo
         if (evn.getSource() == vista.btnModificar) {
             vo.setNit(Integer.parseInt(vista.txtCodigoProveedor.getText()));
             vo.setNombre(vista.txtProveedor.getText());
             vo.setCelular(vista.txtCelular.getText());
             vo.setDireccion(vista.txtDireccion.getText());
         }
-        
+
         if (dao.modificar(vo)) {
-                JOptionPane.showMessageDialog(null, "Registro Guardado");
-                limpiar();
-                llenarCampos();
-                actualizarTabla();
-                
-            } else {
-                JOptionPane.showMessageDialog(null, "Error al Guardar");
-                limpiar();
-                llenarCampos();
-            }
+            JOptionPane.showMessageDialog(null, "Registro Actualizado");
+
+            //Métodos despues de actualizar registro
+            limpiar();
+            llenarCampos();
+            actualizarTabla();
+            
+        //Error si no se actualiza el registro
+        } else {
+            JOptionPane.showMessageDialog(null, "Error al Guardar");
+            
+            //Métodos después del mensaje de erro
+            limpiar();
+            llenarCampos();
+        }
     }
-    
+
+    //Actualiza la tabla cuando se Modifica o guarda un registro
     public void actualizarTabla() {
         ProveedorTablaModelo proveedor = new ProveedorTablaModelo();
         proveedor.mostrarRegistros();
     }
-    
-    public void limpiar(){
+
+    //Limipian los campos de texto
+    public void limpiar() {
         vista.txtCodigoProveedor.setText(null);
         vista.txtProveedor.setText(null);
         vista.txtCelular.setText(null);
         vista.txtDireccion.setText(null);
     }
-    
+
+    //Llenar los campo al actualizar o guardar un registro
     public void llenarCampos() {
         if (vista.txtCodigoProveedor.getText().isEmpty()) {
             vista.txtCodigoProveedor.setText("Código");
@@ -85,5 +95,5 @@ public class ProveedorControladorModificar implements ActionListener{
             vista.txtDireccion.setForeground(new Color(0x666666));
         }
     }
-    
+
 }
