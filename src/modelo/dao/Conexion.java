97e24577a1ae662c8fb265cb3a491d0e7391c580
Comayocode <ronalc726@gmail.com>
2023-01-07 14:14:11 -0500

    feat: Actualizar clase conexion

--- src/modelo/dao/Conexion.java
+++ src/modelo/dao/Conexion.java
@@ -7,8 +7,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Conexion {
-    //?characterEncoding=latin1 ayuda a solucionar el error: "java.sql.SQLException: Unknown initial character set index '255' received from server. Initial client character set can be forced via the 'characterEncoding' property"
-    private final String DB = "gestorinverntario?characterEncoding=latin1";
+    //?characterEncoding=latin1 ayuda a solucionar el error: "java.sql.SQLException: Unknown initial character set index '255' received from server. Initial client character set can be forced via the 'characterEncoding' property" (SE ELIMINÓ)
+    private final String DB = "gestorinventario";
     private final String USER = "root";
     private final String PASSWORD = "5673";
     private final String URL = "jdbc:mysql://localhost:3306/"+DB;
@@ -17,7 +17,7 @@ public class Conexion {
         
     public Connection getConexion(){
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(this.URL, this.USER, this.PASSWORD);
         } catch (SQLException e) {
             //System.err.println("No se pudo conectar" + e);
